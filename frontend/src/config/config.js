d71ed89f9a9f96fc7921af828fc8798096b22f4f
MCIVANILOV <153554972+MCIVANILOV@users.noreply.github.com>
2026-05-05 12:16:06 +0300

    Update config.js

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,7 +1,17 @@
-const host = 'http://localhost:3001/';
+// const host = 'http://localhost:3001/';
+// const config = {
+//     host: host,
+//     api: host + 'api'
+// }
+
+// export default config;
+
+const host = (typeof window !== 'undefined' && (window.location.hostname === 'localhost' ||
+    window.location.hostname === '127.0.0.1')) ? 'http://localhost:3001/' : 'https://lumincoin-finance-vjzt.onrender.com/';
+
 const config = {
     host: host,
-    api: host + 'api'
-}
+    api: host + 'api' // добавляем слеш, чтобы формировать /api/...
+};
 
 export default config;
