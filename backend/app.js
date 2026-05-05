7bc65dc96afbc2fc03dd39dde559442b71a2ffd1
MCIVANILOV <153554972+MCIVANILOV@users.noreply.github.com>
2026-05-05 12:14:38 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const path = require('path'); require('dotenv').config({ path: path.resolve(__dirname, '.env') });
+const path = require('path');
+require('dotenv').config({path: path.resolve(__dirname, '.env')});
 const authRoutes = require('./routes/auth.routes');
 const expenseCategoriesRoutes = require('./routes/category-expense.routes');
 const incomeCategoriesRoutes = require('./routes/category-income.routes');
 const operationsRoutes = require('./routes/operation.routes');
 const balanceRoutes = require('./routes/balance.routes');
 
+const allowedOrigins = ['https://lumincoin-finance-1.onrender.com', 'https://lumincoin-finance-vjzt.onrender.com'];
+
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) !== -1) return callback(null, true);
+        return callback(new Error('Not allowed by CORS'));
+    }, credentials: true
+}));
 
 app.use("/api", authRoutes);
 app.use("/api/categories/expense", expenseCategoriesRoutes);
